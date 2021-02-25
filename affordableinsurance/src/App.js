1c46ab6e29658cbdc1d56731e73a029f66d27d55
danielr9173 <danielr9173@gmail.com>
2021-02-25 17:06:29 -0600

    Daniels Branch Test

--- affordableinsurance/src/App.js
+++ affordableinsurance/src/App.js
@@ -20,7 +20,7 @@ function App() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            AFFORDABLEssss
+            AFFORDABLEdddddd
           </a>
           <span>, </span>
           <a
