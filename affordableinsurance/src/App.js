237f44a54bdc20627dd51b72806bb3eb16196a52
danielr9173 <danielr9173@gmail.com>
2021-02-25 16:45:06 -0600

    Daniels Check

--- affordableinsurance/src/App.js
+++ affordableinsurance/src/App.js
@@ -20,7 +20,7 @@ function App() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            AFFORDABLEsssss
+            AFFORDABLE
           </a>
           <span>, </span>
           <a
