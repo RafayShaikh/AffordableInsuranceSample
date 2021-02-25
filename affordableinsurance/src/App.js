e829421aee6c4ebb754a9131a8b188f2211352d0
Admin <jgarza145@islander.tamucc.edu>
2021-02-25 17:07:07 -0600

    Test Master Commit again

--- affordableinsurance/src/App.js
+++ affordableinsurance/src/App.js
@@ -20,7 +20,7 @@ function App() {
             target='_blank'
             rel='noopener noreferrer'
           >
-            AFFORDABLEssss
+            AFFORDABLeRRR
           </a>
           <span>, </span>
           <a
