431b0b954e15e91249926d97c2de100c9cac83bb
Admin <jgarza145@islander.tamucc.edu>
2021-03-06 18:40:43 -0600

    test

--- affordableinsurance/src/App.js
+++ affordableinsurance/src/App.js
@@ -23,57 +23,6 @@ function App() {
 
   return (
     <div className='App'>
-
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className='App-link'
-            href='https://reactjs.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            asfasdfadfadfasdfsadfa
-          </a>
-          <h1>Rafay</h1>
-          <h1>Daniel</h1>
-          <h1>Joe</h1>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className='App-link'
-            href='https://redux-toolkit.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className='App-link'
-            href='https://react-redux.js.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-
-
       <Header header={header} />
       <div className='Body'>
         <h1>Place Holder</h1>
