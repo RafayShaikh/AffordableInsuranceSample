fb7975676721d1af1c732e7170adb958edf2ad73
danielr9173 <danielr9173@gmail.com>
2021-03-18 01:48:34 -0500

    Contact Page Started

--- affordableinsurance/src/components/Contacts/Contacts.js
+++ affordableinsurance/src/components/Contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './Contacts.css';
 import { withRouter } from 'react-router';
 
 function Contacts() {
@@ -6,7 +7,64 @@ function Contacts() {
     window.scrollTo(0, 0);
   }, []);
 
-  return <div>Contacts</div>;
+  return (
+    <div className='contacts_container'>
+      <div className='contacts_description'>
+        <h1>Contacts Start</h1>
+        <p>
+          Thank you for visiting our website. For all your insurance needs for
+          vehicle, home and office, come to Affordable Insurance of Texas. We
+          have years of experience finding the best rates on the best coverage
+          around! To learn more about any of our programs or about the several
+          companies that we represent, please give us a call today! Our friendly
+          and helpful staff is ready to assist you with your inquiries. (From
+          Original Site)
+        </p>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>We would like to hear from you. Please fill out form below</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+      <div className='contacts_infoForm'>
+        <h2>PLACE HOLDER</h2>
+      </div>
+    </div>
+  );
 }
 
 export default withRouter(Contacts);
