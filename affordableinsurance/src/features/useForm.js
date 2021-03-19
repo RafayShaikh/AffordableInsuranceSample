958851ba92b4332914bfb31976ef2d6013dcbf51
RafayShaikh <rafaysarr@gmail.com>
2021-03-18 20:20:41 -0500

    Update useForm.js
    CHeck

--- affordableinsurance/src/features/useForm.js
+++ affordableinsurance/src/features/useForm.js
@@ -33,12 +33,17 @@ const useForm = (callback, validate) => {
     let check =
       Object.keys(errors).length - Object.keys(validate(values)).length;
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     if (steps == 1 && check == 2) {
 =======
     setErrors(validate(values));
     if (steps == 1 && (check == 2 || check == 0)) {
 >>>>>>> parent of 71b3738... Merge pull request #22 from RafayShaikh/Rafay-Branch
+=======
+    setErrors(validate(values));
+    if (steps == 1 && (check == 2 || check == 0)) {
+>>>>>>> parent of a88404d... FIxed Validation Bug
       setSteps(steps + 1);
     }
     if (steps == 2 && (check == 1 || check == 0)) {
